c3c41f92673efae51b2143d8949c3b8cc6aa22c4
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-14 13:46:49 -0500

    so we can test server

--- server.js
+++ server.js
@@ -6,22 +6,22 @@ const app = express();
 const apiRoutes = require("./routes/apiRoutes");
 
 // Serve up static assets
-app.use(express.static("client/build"));
+// app.use(express.static("client/build"));
 
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: true }));
 
-// parse application/json
-app.use(bodyParser.json());
+// // parse application/json
+// app.use(bodyParser.json());
 
-// Use apiRoutes
-app.use("/api", apiRoutes);
+// // Use apiRoutes
+// app.use("/api", apiRoutes);
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
-});
+// // Send every request to the React app
+// // Define any API routes before this runs
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
