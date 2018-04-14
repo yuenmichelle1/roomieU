e870591a32ae03ef955c2e822ffe69010a944f61
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-14 13:41:17 -0500

    all of it again

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
