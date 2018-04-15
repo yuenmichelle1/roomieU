21268bc8d9f3485975b69b94173ba8b187abc375
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-14 22:59:56 -0500

    restructuring server to make it look like example build

--- server.js
+++ server.js
@@ -1,27 +1,30 @@
-const express = require("express");
 const path = require("path");
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 3001;
 const app = express();
 const apiRoutes = require("./routes/apiRoutes");
+const mongoose = require("mongoose");
+
+// We'll have to change this for production!
+mongoose.connect("mongodb://localhost/product_db");
 
 // Serve up static assets
-// app.use(express.static("client/build"));
+app.use(express.static("client/build"));
 
-// // parse application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// // parse application/json
-// app.use(bodyParser.json());
+// parse application/json
+app.use(bodyParser.json());
 
-// // Use apiRoutes
-// app.use("/api", apiRoutes);
+// Use apiRoutes
+app.use("/api", apiRoutes); //localhost:3000/api/...
 
-// // Send every request to the React app
-// // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
+// Send every request to the React app
+// Define any API routes before this runs
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
