3347d45c69785ba797d186238d10f695cbbbe154
Sean Arca <sean.arca@northwestern.edu>
2018-04-16 15:05:55 -0500

    working navbar & added fake pages

--- client/src/App.js
+++ client/src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import FormWrapper from "./components/FormWrapper";
+import NavBar from "./components/NavBar";
+import Home from "./components/Home";
+import SignupForm from "./components/SignupForm";
+import Login from "./components/Login";
 
 
 class App extends Component {
   render() {
     return (
       <Router>
-      <div>
-        {/* <Route exact path="/" component={Home} />
-        <Route exact path="/signup" component={SignupForm} /> */}
+      <div className="container-fluid">
+        <NavBar />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/signup" component={SignupForm} />
+        <Route exact path="/login" component={Login} />
         <Route exact path="/roommatepreferences" component={FormWrapper} />
         {/* <Route exact path="/dashboard" component={Dashboard} /> */}
       </div>
