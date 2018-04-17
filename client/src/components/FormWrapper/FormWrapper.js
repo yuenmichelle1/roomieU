acd756eb375c3b3ccaffbcd8916ea37f1e870044
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-17 00:34:27 -0500

    making userProfile Questions

--- client/src/components/FormWrapper/FormWrapper.js
+++ client/src/components/FormWrapper/FormWrapper.js
@@ -1,9 +1,12 @@
 import React from "react";
 import RoomieForm from "../RoomieForm";
+import UserinfoForm from "../UserinfoForm";
 
 const FormWrapper = () => {
   return (
     <div className="wrapper">
+      <h1 className="text-center"> Tell Us A Bit About You</h1>
+      <UserinfoForm />
       <h1 className="text-center"> Your Ideal Roommate</h1>
       <RoomieForm />
     </div>
