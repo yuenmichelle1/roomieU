1b403be2e9fe257608d373bd7b60c841825f7177
Sean Arca <sean.arca@northwestern.edu>
2018-04-18 21:00:01 -0500

    hero section complete

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import FormWrapper from "./components/FormWrapper";
-// import NavBar from "./components/NavBar";
+import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SignupForm from "./components/SignupForm";
 import Login from "./components/Login";
@@ -13,7 +13,7 @@ class App extends Component {
     return (
       <Router>
       <div className="container-fluid">
-        {/* <NavBar /> */}
+        <NavBar />
         <Route exact path="/" component={Home} />
         <Route exact path="/signup" component={SignupForm} />
         <Route exact path="/login" component={Login} />
