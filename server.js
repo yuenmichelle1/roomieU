4a4e1716a4ba0345e07238c89aceafb603d8d2f7
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-20 10:37:57 -0500

    ok

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 require("dotenv").config();
 // you will need to set up your local .env file to access the key since .env file is ignored by gitignore
-console.log(process.env.ZillowApiKey)
-
+console.log(process.env.ZillowApiKey);
 
 const express = require("express");
 const path = require("path");
@@ -16,26 +15,35 @@ mongoose.Promise = global.Promise;
 
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/roomies_db";
 
-mongoose.connect(MONGODB_URI)
-    .then(() => console.log("connection to mongodb succesful"))
-    .catch(err => console.log(err));
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => console.log("connection to mongodb succesful"))
+  .catch(err => console.log(err));
 
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const session = require("express-session");
-app.use(session({secret: "keyboard cat", resave: false, saveUninitialized: false}))
+app.use(
+  session({ secret: "keyboard cat", resave: false, saveUninitialized: false })
+);
 app.use(passport.initialize());
 app.use(passport.session());
 
 const User = require("./models").User;
 // passport.use(new LocalStrategy(User.authenticate()));
-passport.use(User.createStrategy())
+passport.use(User.createStrategy());
 passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser())
-
+passport.deserializeUser(User.deserializeUser());
 
 // Serve up static assets
-app.use(express.static(path.join(__dirname,"client", "build")));
+// app.use(express.static(path.join(__dirname,"client", "build")));
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+} else {
+  // Use express.static to serve the public folder as a static directory
+  app.use(express.static("public"));
+}
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -48,10 +56,12 @@ app.use(bodyParser.json());
 
 // // Send every request to the React app
 // // Define any API routes before this runs
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+// });
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
-
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
