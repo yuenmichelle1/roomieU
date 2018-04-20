a4b722468cc7d77479862487834112d61f934a6f
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-19 21:59:23 -0500

    initial commit

---
+++ client/src/components/QuestionCard/QuestionCard.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { RadioGroup, ReversedRadioButton } from "react-radio-buttons";
+
+const QuestionCard = () => {
+    return(
+        <RadioGroup />
+    )
+
+}
+
+export default QuestionCard;
