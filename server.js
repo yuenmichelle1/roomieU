d617ba0c8a6184336fa2e56479cd0db4501083e9
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-20 10:24:37 -0500

    again

--- server.js
+++ server.js
@@ -48,9 +48,10 @@ app.use(bodyParser.json());
 
 // // Send every request to the React app
 // // Define any API routes before this runs
-app.get("/", function(req, res) {
-  res.sendFile(path.join("client", "build", "index.html"));
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
+
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
