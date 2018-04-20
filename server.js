a442c237e06b9e077e4363047c394451dbe87a36
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-19 22:24:05 -0500

    ok

--- server.js
+++ server.js
@@ -49,7 +49,7 @@ app.use(routes); //localhost:3000/api/...
 // // Send every request to the React app
 // // Define any API routes before this runs
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  res.sendFile(path.join(__dirname, "client", "public", "index.html"));
 });
 
 app.listen(PORT, function() {
