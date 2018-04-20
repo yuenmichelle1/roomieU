bc613ee50e0c4a5b711e688ab33f3db183e094dd
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-20 10:40:18 -0500

    again with scott

--- server.js
+++ server.js
@@ -36,14 +36,14 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 // Serve up static assets
-// app.use(express.static(path.join(__dirname,"client", "build")));
+app.use(express.static(path.join(__dirname,"client", "build")));
 // Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-} else {
-  // Use express.static to serve the public folder as a static directory
-  app.use(express.static("public"));
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static("client/build"));
+// } else {
+//   // Use express.static to serve the public folder as a static directory
+//   app.use(express.static("public"));
+// }
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -56,12 +56,12 @@ app.use(bodyParser.json());
 
 // // Send every request to the React app
 // // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-// });
 app.get("*", function(req, res) {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
+// app.get("*", function(req, res) {
+//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
+// });
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
