f99c88d99a12ace61720dd0930e80a1a365c0319
Marylin <marylin@Marylins-MacBook-Pro.local>
2018-04-30 09:33:21 -0500

    push first then test quan branch

---
+++ client/src/components/LikedApartmentsWrapper/LikedApartmentsWrapper.js
@@ -0,0 +1,15 @@
+import React, {Component} from "react";
+import API from "../../utils/API";
+
+class LikedApartmentsWrapper extends Component {
+    state={
+        apartments:[]
+    }
+    // grab liked apartments form user then push to state
+    componentDidMount() {
+        
+    }
+
+}
+
+export default LikedApartmentsWrapper;
