d6c6399cfa6216d8bc3766e73b88ea51f8f852c1
Quan Gao <30275919+QuanGao@users.noreply.github.com>
2018-05-01 19:29:27 -0500

    add pic in navbar

--- client/src/components/LogoutBtn/LogoutBtn.js
+++ client/src/components/LogoutBtn/LogoutBtn.js
@@ -9,6 +9,7 @@ import {
 const LogoutBtn = (props) => (
     <Nav className="ml-auto" navbar>
         <NavItem> Welcome {props.userInfo.name} </NavItem>
+        <img src={props.userInfo.photo} style={{width:"40px", height:"40px"}}/>
         <NavLink href="/"><Button onClick = {props.handleLogout} color="warning" id="login-btn" className="btns login-btn">Log Out</Button></NavLink>
     </Nav>
 )
