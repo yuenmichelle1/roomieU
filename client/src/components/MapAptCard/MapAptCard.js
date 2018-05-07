b828877e53d0743e7ea07b4290359e27b349a134
yuenmichelle1 <michelle.dano.yuen@gmail.com>
2018-05-07 01:40:37 -0500

    clean up console logs

--- client/src/components/MapAptCard/MapAptCard.js
+++ client/src/components/MapAptCard/MapAptCard.js
@@ -2,23 +2,36 @@ import {
   Card,
   CardImg,
   CardBody,
-  Button
+  Button,
+  UncontrolledCarousel
 } from "reactstrap";
 import React from "react";
 
+
+
 const MapAptCard = props => {
-  console.log(props.isSaved + "HALLLELUJA IF IT IS FALSE")
+  const items=props.aptData.imageURLs.map(image => ({
+    src: image,
+    altText: "Slide", 
+    caption: 'Slide',
+    header: 'Header'
+  }));
   return (
     <div>
       <Card>
-        <CardImg
-          src={
-            props.aptData.imageURLs
-              ? props.aptData.imageURLs[0]
-              : "https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"
-          }
+        {/* <CardImg
+          src={props.aptData.imageURLs ? props.aptData.imageURLs[0] : "https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180"}
           alt="Card image cap"
-        />
+        /> */}
+        <UncontrolledCarousel
+          autoPlay={true}
+          items={props.aptData.imageURLs ? (props.aptData.imageURLs.map(image => ({
+            src: image,
+            altText: "Apartment", 
+            caption: 'Slide',
+            header: 'Header'
+          }))) : ({src: "https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180", altText: 'no photo found', caption: "No Photo Found", header: "No Photo Found"})} />
+        
         <CardBody>
           <h1>
             {props.aptData.address}, {props.aptData.city}
